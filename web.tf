7db0a6966b19394aec68fe38ade160f0ca3e1c37
hashitop <50546939+hashitop@users.noreply.github.com>
2019-06-11 14:36:17 +1000

    Update web.tf

--- web.tf
+++ web.tf
@@ -4,6 +4,10 @@ variable "access_key" {}
 
 variable "secret_key" {}
 
+terraform {
+  required_version = ">= 0.11, < 0.12"
+}
+
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
